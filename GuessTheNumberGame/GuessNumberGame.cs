a3011b631a86236e036e58c37d767126e2593fcd
Chris Sequeira <chris.seq17@gmail.com>
2022-05-22 09:57:46 +1200

    expanding the game to change the range. Also moved game to it's own class

---
+++ GuessTheNumberGame/GuessNumberGame.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Globalization;
+
+namespace GuessTheNumberGame
+{
+    public class GuessNumberGame
+    {
+        private const int DEFAULTRANDOMLIMIT = 20;
+        private int _randomNumber;
+        private int _customLimit;
+        private bool _keepPlaying { get; set; }
+        
+        public GuessNumberGame()
+        {
+            _randomNumber = new Random().Next(DEFAULTRANDOMLIMIT);
+            _keepPlaying = true;
+            _customLimit = DEFAULTRANDOMLIMIT;
+        }
+
+        public void PlayGame()
+        {
+            var guessesCount = 0;
+            InitializeGreeting();
+
+            do
+            {
+                guessesCount++;
+                Console.WriteLine("What's your guess?");
+                string guess = Console.ReadLine();
+
+                if (int.TryParse(guess, NumberStyles.Number, null, out var guessNum))
+                {
+                    Console.WriteLine(DetermineGuessResponse(guessNum, guessesCount));
+                }
+                else
+                {
+                    Console.WriteLine("Hmmm, that doesn't look like a number. Try again.");
+                }
+            }
+            while (_keepPlaying);
+        }
+
+        private void InitializeGreeting()
+        {
+            Console.WriteLine("Let's Play 'Guess the Number'!");
+            ChangeLimit();
+            Console.WriteLine($"I'm thinking of a number between 0 and {_customLimit}.");
+            Console.WriteLine("Enter your guess, or -1 to give up.");
+        }
+
+        private void ChangeLimit()
+        {
+            Console.WriteLine($"Would you like to change the range limit from {_customLimit}? Y/N");
+            var answer = Console.ReadLine();
+
+            do
+            {
+                if (answer.ToUpper() == "Y")
+                {
+                    Console.WriteLine("Enter a number to change the range limit");
+                    string numLimitEntered = Console.ReadLine();
+                    if (int.TryParse(numLimitEntered, NumberStyles.Number, null, out _customLimit))
+                    {
+                        _randomNumber = new Random().Next(_customLimit);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hmmm, that doesn't look like a number. Try again.");
+                    }
+                }
+            }
+            while (answer.ToUpper() != "Y" && answer.ToUpper() != "N");
+        }
+
+        private string DetermineGuessResponse(int guessNum, int guessCount)
+        {
+            _keepPlaying = guessNum == -1 || guessNum == _randomNumber ? false : true;
+            var guessAgainText = $"Nope, {(guessNum > _randomNumber ? "lower" : "higher")} than that.";
+            return guessNum == _randomNumber && guessNum != -1 ? $"You got it in {guessCount} guesses"
+                        : !_keepPlaying ? $"Oh well I was thinking of {_randomNumber}"
+                        : guessAgainText;
+        }
+    }
+}
--- GuessTheNumberGame/Program.cs
+++ GuessTheNumberGame/Program.cs
@@ -10,42 +10,8 @@ namespace GuessTheNumberGame
     {
         static void Main(string[] args)
         {
-            // Choose a random number between 0 and 20
-            var theNumber = new Random().Next(20);                       
-            var keepPlaying = true;
-
-            // Print the game greeting and instructions
-            Console.WriteLine("Let's Play 'Guess the Number'!");
-            Console.WriteLine("I'm thinking of a number between 0 and 20.");
-            Console.WriteLine("Enter your guess, or -1 to give up.");
-
-            // Keep track of the number of guesses and the current user guess
-            var guessesCount = 0;
-            int guessNum;
-
-            // play the game atleast once
-            do
-            {
-                guessesCount++;
-                Console.WriteLine("What's your guess?");
-                string guess = Console.ReadLine();
-                
-                if (int.TryParse(guess, NumberStyles.Number, null, out guessNum))
-                {
-                    keepPlaying = guessNum == -1 || guessNum == theNumber ? false : true;
-                    var guessAgainText = "Nope, " + (guessNum > theNumber ? "lower " : "higher ") + "than that.";
-                    var outputText = guessNum == theNumber && guessNum != -1 ? $"You got it in {guessesCount} guesses"                        
-                        : !keepPlaying ? $"Oh well I was thinking of {theNumber}"
-                        : guessAgainText;
-                   
-                    Console.WriteLine(outputText);
-                }
-                else
-                {
-                    Console.WriteLine("Hmmm, that doesn't look like a number. Try again.");
-                }
-            }
-            while (keepPlaying);
+            var numberGame = new GuessNumberGame();
+            numberGame.PlayGame();
         }
     }
 }
